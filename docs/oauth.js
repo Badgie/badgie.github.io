dfbfbb1b619ab6e3b58fb7f13318acf079321f1b
Badgy <jonaskhansen@gmail.com>
2019-12-05 12:34:54 +0100

    update redirect

--- docs/oauth.js
+++ docs/oauth.js
@@ -2,7 +2,7 @@ OAUTH_AUTH_URL = 'https://accounts.spotify.com/authorize';
 OAUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token';
 CLIENT_ID = '78ba9bb95f5b40a2b74bc7733ea3cd81';
 CLIENT_SECRET = 'a0189d34e14d44448ddd9ed71d46e6f7';
-REDIRECT_URL = 'http://localhost:63342/spotirec.me/auth.html';
+REDIRECT_URL = 'https://badgie.github.io/docs/auth.html';
 SCOPES = 'user-top-read playlist-modify-public playlist-modify-private user-read-private user-read-email ' +
     'ugc-image-upload user-read-playback-state user-modify-playback-state user-library-modify';
 
