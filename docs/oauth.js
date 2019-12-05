a52df187ab99b236ea8190dd513ceb288571e92f
Badgy <jonaskhansen@gmail.com>
2019-12-05 14:39:21 +0100

    change to implicit grant

--- docs/oauth.js
+++ docs/oauth.js
@@ -1,13 +1,14 @@
-OAUTH_AUTH_URL = 'https://accounts.spotify.com/authorize';
-OAUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token';
-CLIENT_ID = '78ba9bb95f5b40a2b74bc7733ea3cd81';
-CLIENT_SECRET = 'a0189d34e14d44448ddd9ed71d46e6f7';
-REDIRECT_URL = 'https://badgie.github.io/docs/auth.html';
-SCOPES = 'user-top-read playlist-modify-public playlist-modify-private user-read-private user-read-email ' +
+const OAUTH_AUTH_URL = 'https://accounts.spotify.com/authorize';
+const OAUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token';
+const CLIENT_ID = '78ba9bb95f5b40a2b74bc7733ea3cd81';
+const CLIENT_SECRET = 'a0189d34e14d44448ddd9ed71d46e6f7';
+const REDIRECT_URL = 'https://badgie.github.io/docs/auth.html';
+const SCOPES = 'user-top-read playlist-modify-public playlist-modify-private user-read-private user-read-email ' +
     'ugc-image-upload user-read-playback-state user-modify-playback-state user-library-modify';
+let STATE = randomString(10);
 
 function auth() {
-    let params = {'client_id': CLIENT_ID, 'response_type': 'code', 'redirect_uri': REDIRECT_URL, 'scope': SCOPES};
+    let params = {'client_id': CLIENT_ID, 'response_type': 'token', 'redirect_uri': REDIRECT_URL, 'scope': SCOPES, 'state': STATE};
     let serialize = function () {
         let str = [];
         for (let field in params) {
@@ -23,29 +24,21 @@ function getCode() {
     let code = url.split('?code=')[1];
     let body = JSON.stringify({'grant_type': 'authorization_code', 'code': code, 'redirect_uri': REDIRECT_URL});
     let secret = btoa(CLIENT_ID + ':' + CLIENT_SECRET);
-    //let http = new XMLHttpRequest();
-    $.ajax({
-        type: 'post',
-        url: OAUTH_TOKEN_URL,
-        contentType: 'application/json',
-        data: body,
-        dataType: 'json',
-        beforeSend: function (request) {
-            request.setRequestHeader('Authorization', 'Basic ' + secret);
-        },
-        success: function (result) {
-            updateDatabase(result);
-        },
-        async: true
-    });
-    /*http.onreadystatechange = function() {
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = function() {
         if (http.readyState === 4 && http.status === 200) {
             updateDatabase(JSON.parse(http.responseText));
         }
     };
     http.open('POST', OAUTH_TOKEN_URL, true);
     http.setRequestHeader('Authorization', 'Basic ' + secret);
-    http.send(body);*/
+    http.send(body);
+}
+
+function getCredentials() {
+    let url = window.location.href;
+    let credentials = url.split('#')[1].split('&');
+    document.getElementById('yeet').innerHTML = credentials.join(', ')
 }
 
 function updateDatabase(token) {
@@ -54,4 +47,14 @@ function updateDatabase(token) {
         token['scope'] + ', ' +
         token['expires_in'] + ', ' +
         token['refresh_token'];
+}
+
+function randomString(length) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
